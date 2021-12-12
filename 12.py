e0d00357d04814819eee0eaa9cf0540084df8a2c
Aayush Lamichhane <lamichhaneaayush5@gmail.com>
2021-12-12 17:16:30 +0545

    make faster ⏩

--- 12.py
+++ 12.py
@@ -19,11 +19,11 @@ def part1(lines):
     def find(paths, beginning, visited):
         nonlocal count
         for path in paths[beginning]:
-            dup = copy.deepcopy(visited)
+            dup = copy.copy(visited)
             dup.append(path)
             if path == "end":
                 count += 1
-                print(dup)
+                (dup)
             elif path.islower() and path not in visited or path.isupper():
                 find(paths, path, dup)
 
@@ -35,33 +35,32 @@ def part2(lines):
     paths = {}
     for line in lines:
         start, end = line.split("-")
-        if start in paths:
-            paths[start].append(end)
-        else:
-            paths[start] = [end]
-        if end in paths:
-            paths[end].append(start)
-        else:
-            paths[end] = [start]
+        if end != "start":
+            if start in paths:
+                paths[start].append(end)
+            else:
+                paths[start] = [end]
+        if start != "start":
+            if end in paths:
+                paths[end].append(start)
+            else:
+                paths[end] = [start]
 
     count = 0
 
     def find(paths, beginning, visited, small_cave):
         nonlocal count
         for path in paths[beginning]:
-            dup = copy.deepcopy(visited)
-            dup.append(path)
-            if path == "start":
-                continue
-            elif path == "end":
+            if path == "end":
                 count += 1
-            elif path.isupper():
-                find(paths, path, dup, small_cave)
+                continue
+            if path.isupper():
+                find(paths, path, copy.copy(visited) + [path], small_cave)
             elif path.islower():
                 if path not in visited:
-                    find(paths, path, dup, small_cave)
+                    find(paths, path, copy.copy(visited) + [path], small_cave)
                 elif small_cave:
-                    find(paths, path, dup, False)
+                    find(paths, path, copy.copy(visited) + [path], False)
 
     find(paths, "start", ["start"], True)
     print(count)
@@ -70,5 +69,5 @@ def part2(lines):
 if __name__ == "__main__":
     with open("./input/day12.txt", "r") as f:
         lines = [a.strip() for a in f.readlines()]
-    # part1(lines)
+    part1(lines)
     part2(lines)
